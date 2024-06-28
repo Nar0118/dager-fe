13645c6d8f0ab92108d6ec4d00f19fd8be5b11ce
Narek Khachatryan <narek@Narek-Khachatryans-MacBook-Pro.local>
2024-06-28 11:11:37 +0400

    made home page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AdminPage from "./pages/AdminPage";
 import Home from "./pages/Home";
@@ -7,8 +7,28 @@ import LoginPage from "./pages/Login";
 import NotFound from "./pages/404";
 import { Navbar } from "./components/Navbar";
 import CarDetails from "./pages/CarDetails";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      console.log(6666666, 'hasav');
+      try {
+        const response = await fetch('https://www.jnbk-brakes.com/application/get_applications/5427/1/0/0/0?body_value=0&eng_no=0');
+        // if (!response.ok) {
+        //   throw new Error('Network response was not ok');
+        // }
+        console.log(66666, response);
+        const html = await response.text();
+        console.log(66666, html);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <Router>
       <Navbar />
