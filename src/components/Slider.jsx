d407d55b6df7053602652403530f2589282a35e1
Nar0118 <khachatryannarek42@gmail.com>
2024-08-02 14:21:49 +0400

    changed slide images

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -4,29 +4,29 @@ import "./slider.css";
 
 const slides = [
   {
-    city: "Paris",
-    country: "France",
-    img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/paris.jpg",
+    city: "Pads",
+    // country: "Brake",
+    img: "/images/pads-bg.png",
   },
   {
-    city: "Singapore",
-    img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/singapore.jpg",
+    city: "Wiper blades",
+    img: "/images/conventional-wiper.jpg",
   },
   {
-    city: "Prague",
-    country: "Czech Republic",
-    img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/prague.jpg",
+    city: "Spark plugs",
+    // country: "Czech Republic",
+    img: "/images/Spark-Plug.jpg",
   },
-  {
-    city: "Amsterdam",
-    country: "Netherlands",
-    img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/amsterdam.jpg",
-  },
-  {
-    city: "Moscow",
-    country: "Russia",
-    img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/moscow.jpg",
-  },
+  // {
+  //   city: "Amsterdam",
+  //   country: "Netherlands",
+  //   img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/amsterdam.jpg",
+  // },
+  // {
+  //   city: "Moscow",
+  //   country: "Russia",
+  //   img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/moscow.jpg",
+  // },
 ];
 
 class Slider extends React.Component {
